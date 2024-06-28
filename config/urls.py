990f3050cccdc67fa08604536b0adcee125ff643
ILKHOM <gaffarovilkhom@inbox.ru>
2024-06-28 13:19:16 +0500

    Add celery

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,34 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('habits/', include('habits.urls', namespace='habits')),
-    path('users/', include('users.urls', namespace='users')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('habits/', include('habits.urls', namespace='habits')),
+                  path('users/', include('users.urls', namespace='users')),
+                  path(
+                      "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+                  ),
+                  path(
+                      "swagger/",
+                      schema_view.with_ui("swagger", cache_timeout=0),
+                      name="schema-swagger-ui",
+                  ),
+                  path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
