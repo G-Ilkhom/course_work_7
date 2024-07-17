0737ca696d52c871e59d91a727edd688da673399
ILKHOM <gaffarovilkhom@inbox.ru>
2024-07-17 20:07:29 +0300

    Separate containers have been created for Django, PostgreSQL, Redis and Celery services.

--- config/settings.py
+++ config/settings.py
@@ -65,9 +65,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("NAME"),
-        "USER": os.getenv("USER"),
-        "PASSWORD": os.getenv("PASSWORD"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
